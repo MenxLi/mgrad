9da741b710390f07303f8cac7375028579a387b6
limengxun <limengxun45@outlook.com>
2024-10-31 13:11:22 +0800

    draw bitmap

--- demo_mlp.cc
+++ demo_mlp.cc
@@ -1,4 +1,6 @@
 #include "src/nn_blocks.h"
+#include "utils/bitmap.h"
+
 #include <cstdlib>
 #include <random>
 #include <iostream>
@@ -59,7 +61,8 @@ Model create_model(nn::Graph& graph){
 
     auto& prediciton = *l3.output[0];
     prediciton.name = "prediction";
-    auto& bce_loss = -output_aim * prediciton.log() - (1 - output_aim) * (1 - prediciton).log();
+    const nn::fp_t eps = 1e-7;
+    auto& bce_loss = -output_aim * (prediciton + eps).log() - (1 - output_aim) * (1 - prediciton + eps).log();
 
     return Model{
         &graph,
@@ -116,23 +119,27 @@ void train_step(Model& model, int n_iter, int total_iter){
         model.graph->nodes[i]->value -= lr * grad;
     }
 
-    if ((n_iter + 1) % (int)1e3 == 0) {
+    if ((n_iter + 1) % (int)1e4 == 0) {
         std::cout << "Iteration [" << n_iter + 1 << "/" << total_iter << "]"
         << "\t loss: " << loss / batch_size << std::endl;
     }
     model.graph->clear_grad();
 }
 
+void save_bitmap(Model& model);
+
 int main(){
     nn::Graph graph;
 
     Model model = create_model(graph);
-    const int total_iter = 5e4;
+    const int total_iter = 1e5;
     for (int i = 0; i < total_iter; i++){
         train_step(model, i, total_iter);
     }
 
     std::cout << "final loss: " << model.loss->value << ", acc: " << get_acc(model) << std::endl;
+
+    save_bitmap(model);
 
     // input a simple value and save the graph
     model.input_x->value = 1;
@@ -145,4 +152,42 @@ int main(){
     ofs.close();
     
     return 0;
+}
+
+// ================== Utility for visualization ==================
+void save_bitmap(Model& model){
+    const int w = 256;
+    const int h = 256;
+    float res[w][h];
+
+    auto norm_value = [](nn::fp_t v){
+        // map to -1 - 2, for visualization
+        if (v < -1) v = -1;
+        if (v > 2) v = 2;
+        v = (v + 1) / 3;
+        return (unsigned char)(v * 255);
+    };
+
+    for (int i = 0; i < w; i++){
+        for (int j = 0; j < h; j++){
+            model.input_x->value = i * 10.0 / w - 5;
+            model.input_y->value = j * 10.0 / h - 5;
+            model.graph->forward();
+            auto pred = model.prediciton->value;
+            res[i][j] = norm_value(pred);
+        }
+    }
+    write_bitmap<w, h>("mlp_prediction.bmp", res, res, res);
+
+    for (int i = 0; i < w; i++){
+        for (int j = 0; j < h; j++){
+            auto z = static_cast<nn::fp_t>(
+                aim_levelset(i * 10.0 / w - 5, j * 10.0 / h - 5) < 0
+                );
+            res[i][j] = norm_value(z);
+            res[i][j] = norm_value(z);
+            res[i][j] = norm_value(z);
+        }
+    }
+    write_bitmap<w, h>("mlp_aim.bmp", res, res, res);
 }
